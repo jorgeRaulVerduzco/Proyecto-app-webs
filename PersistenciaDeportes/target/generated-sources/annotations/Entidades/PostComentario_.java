1707b5170405f9ad992a1db1230f8f89a624ba22
jorgeRaulVerduzco <jorge.verduzco247253@potros.itson.edu.mx>
2024-11-22 21:34:57 -0700

    cambiar contraseña en el persistence password

--- PersistenciaDeportes/target/generated-sources/annotations/Entidades/PostComentario_.java
+++ PersistenciaDeportes/target/generated-sources/annotations/Entidades/PostComentario_.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-11-22T21:27:55", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-11-22T21:34:35", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(PostComentario.class)
 public class PostComentario_ { 
 
