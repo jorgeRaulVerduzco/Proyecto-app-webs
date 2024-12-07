c4efa6921025d576995783e4d7ac392f92569cbc
Esdras Copado <copadoe6@gmail.com>
2024-12-07 13:17:09 -0700

    guardado

--- PersistenciaDeportes/target/generated-sources/annotations/Entidades/Post_.java
+++ PersistenciaDeportes/target/generated-sources/annotations/Entidades/Post_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-12-06T13:33:21", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-12-07T13:12:17", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Post.class)
 public class Post_ { 
 
